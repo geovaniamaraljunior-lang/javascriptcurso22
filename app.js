bc0f6d5b86fc2f83a4ab6ef0298fc5580b6406a4
geovaniamaraljunior-lang <geovani.amaral.junior@escola.pr.gov.br>
2025-10-21 11:02:00 -0300

    ok

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
